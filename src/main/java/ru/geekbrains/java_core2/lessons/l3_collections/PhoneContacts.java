990f638bbefad0b421c8f224525e0d3ef72ad503
OdaryMe <elenanevsk@gmail.com>
2024-08-06 15:03:21 +0300

    Исправила

--- src/main/java/ru/geekbrains/java_core2/lessons/l3_collections/PhoneContacts.java
+++ src/main/java/ru/geekbrains/java_core2/lessons/l3_collections/PhoneContacts.java
@@ -12,7 +12,7 @@ public class PhoneContacts {
 
     public void add(String name, String number) {
         List<String> list = contacts.get(name);
-        if (contacts.get(name) == null) {
+        if (list == null) {
             list = new ArrayList<>();
             list.add(number);
             contacts.put(name, list);
@@ -22,7 +22,6 @@ public class PhoneContacts {
     }
 
     public List<String> get(String name) {
-        List<String> list = contacts.get(name);
-        return list;
+        return contacts.get(name);
     }
 }
